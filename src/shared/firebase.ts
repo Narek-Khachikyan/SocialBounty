590ef4024445611145be60d0399e555c80d690fa
magiccwarss <matem.mayreni@gmail.com>
2025-05-18 22:07:33 +0400

    feat: add Firebase database initialization to firebase.ts

--- src/shared/firebase.ts
+++ src/shared/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCmrhOboe6ZHYW5c9R0b8Hd0MhN90Yndfk",
@@ -8,7 +9,10 @@ const firebaseConfig = {
   storageBucket: "soical-bounty.firebasestorage.app",
   messagingSenderId: "40419418635",
   appId: "1:40419418635:web:c7a61cf850020de28bdd0e",
+  databaseURL:
+    "https://soical-bounty-default-rtdb.europe-west1.firebasedatabase.app/",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const database = getDatabase(app);
